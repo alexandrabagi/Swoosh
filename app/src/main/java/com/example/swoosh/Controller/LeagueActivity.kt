8500742631e3fe3cda08e2dbbc93a5d594d49ad0
AlexandraBagi <szandra.bagi@gmail.com>
2019-11-19 19:09:57 +0100

    Player class passed around and launcher icon added

--- app/src/main/java/com/example/swoosh/Controller/LeagueActivity.kt
+++ app/src/main/java/com/example/swoosh/Controller/LeagueActivity.kt
@@ -2,15 +2,31 @@ package com.example.swoosh.Controller
 
 import android.content.Intent
 import android.os.Bundle
+import android.os.PersistableBundle
 import android.view.View
 import android.widget.Toast
-import com.example.swoosh.Utilities.EXTRA_LEAGUE
+import com.example.swoosh.Model.Player
 import com.example.swoosh.R
+import com.example.swoosh.Utilities.EXTRA_PLAYER
 import kotlinx.android.synthetic.main.activity_league.*
 
 class LeagueActivity : BaseActivity() {
 
-    var selectedLeague = ""
+    var player = Player("", "")
+
+    // we save our current state into outstate when we rotate
+    override fun onSaveInstanceState(outState: Bundle?) {
+        super.onSaveInstanceState(outState)
+        // safe call on outstate because Bundle is nullable
+        outState?.putParcelable(EXTRA_PLAYER, player)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle?) {
+        super.onRestoreInstanceState(savedInstanceState)
+        if (savedInstanceState != null) {
+            player = savedInstanceState.getParcelable(EXTRA_PLAYER)
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,27 +42,27 @@ class LeagueActivity : BaseActivity() {
         womensButton.isChecked = false
         coedButton.isChecked = false
 
-        selectedLeague = "mens"
+        player.league = "mens"
     }
 
     fun onWomensClicked(view: View) {
         mensButton.isChecked = false
         coedButton.isChecked = false
 
-        selectedLeague = "womens"
+        player.league = "womens"
     }
 
     fun onCoedClicked(view: View) {
         mensButton.isChecked = false
         womensButton.isChecked = false
 
-        selectedLeague = "co-ed"
+        player.league = "co-ed"
     }
 
     fun leagueNextClicked(view: View) {
-        if (selectedLeague != "") {
+        if ( player.league != "") {
             val skillIntent = Intent(this, SkillActivity::class.java)
-            skillIntent.putExtra(EXTRA_LEAGUE, selectedLeague)
+            skillIntent.putExtra(EXTRA_PLAYER, player)
             startActivity(skillIntent)
         } else {
             Toast.makeText(this, "Please select a league!", Toast.LENGTH_SHORT)
--- app/src/main/java/com/example/swoosh/Controller/SkillActivity.kt
+++ app/src/main/java/com/example/swoosh/Controller/SkillActivity.kt
@@ -4,40 +4,50 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
-import com.example.swoosh.Utilities.EXTRA_LEAGUE
+import com.example.swoosh.Model.Player
 import com.example.swoosh.R
-import com.example.swoosh.Utilities.EXTRA_SKILL
+import com.example.swoosh.Utilities.EXTRA_PLAYER
 import kotlinx.android.synthetic.main.activity_skill.*
 
 class SkillActivity : BaseActivity() {
 
-    var league = ""
-    var skill = ""
+    lateinit var player : Player
+
+    override fun onSaveInstanceState(outState: Bundle?) {
+        super.onSaveInstanceState(outState)
+        // safe call on outstate because Bundle is nullable
+        outState?.putParcelable(EXTRA_PLAYER, player)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle?) {
+        super.onRestoreInstanceState(savedInstanceState)
+        if (savedInstanceState != null) {
+            player = savedInstanceState.getParcelable(EXTRA_PLAYER)
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_skill)
-        league = intent.getStringExtra(EXTRA_LEAGUE)
-        println(league)
+        player = intent.getParcelableExtra(EXTRA_PLAYER)
     }
 
     fun onBeginnerClicked(view: View) {
         ballerButton.isChecked = false
 
-        skill = "beginner"
+        player.skill = "beginner"
     }
 
     fun onBallerClicked(view: View) {
         beginnerButton.isChecked = false
 
-        skill = "baller"
+        player.skill = "baller"
     }
 
     fun onFinishSkillClicked(view: View) {
-        if (skill != "") {
+        if (player.skill != "") {
             val finishIntent = Intent(this, FinishActivity::class.java)
-            finishIntent.putExtra(EXTRA_LEAGUE, league)
-            finishIntent.putExtra(EXTRA_SKILL, skill)
+            finishIntent.putExtra(EXTRA_PLAYER, player)
             startActivity(finishIntent)
         } else {
             Toast.makeText(this, "Please select a skill level!", Toast.LENGTH_SHORT)
