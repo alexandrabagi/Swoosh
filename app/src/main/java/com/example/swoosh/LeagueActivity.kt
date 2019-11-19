3b4c49e8f9718d5a031ec3c5f09e8e2d6e096e26
AlexandraBagi <szandra.bagi@gmail.com>
2019-11-19 17:32:48 +0100

    Wiring up buttons in LeagueActivity

--- app/src/main/java/com/example/swoosh/LeagueActivity.kt
+++ app/src/main/java/com/example/swoosh/LeagueActivity.kt
@@ -4,9 +4,12 @@ import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_league.*
 
 class LeagueActivity : BaseActivity() {
+
+    var selectedLeague = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,8 +21,36 @@ class LeagueActivity : BaseActivity() {
 //        }
     }
 
+    fun onMensClicked(view: View) {
+        womensButton.isChecked = false
+        coedButton.isChecked = false
+
+        selectedLeague = "mens"
+    }
+
+    fun onWomensClicked(view: View) {
+        mensButton.isChecked = false
+        coedButton.isChecked = false
+
+        selectedLeague = "womens"
+    }
+
+    fun onCoedClicked(view: View) {
+        mensButton.isChecked = false
+        womensButton.isChecked = false
+
+        selectedLeague = "co-ed"
+    }
+
     fun leagueNextClicked(view: View) {
-        val skillIntent = Intent(this, SkillActivity::class.java)
-        startActivity(skillIntent)
+        if (selectedLeague != "") {
+            val skillIntent = Intent(this, SkillActivity::class.java)
+            skillIntent.putExtra(EXTRA_LEAGUE, selectedLeague)
+            startActivity(skillIntent)
+        } else {
+            Toast.makeText(this, "Please select a league!", Toast.LENGTH_SHORT)
+                .show()
+        }
+
     }
 }
